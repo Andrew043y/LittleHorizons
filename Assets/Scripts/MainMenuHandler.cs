4f2b49a64b9046bc7009fe25cdc720f39ef661cf
Andrew Kolb <andrew043y@yahoo.com>
2025-04-21 17:13:54 -0500

    update

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,11 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenuHandler : MonoBehaviour
 {
     public AudioMixer masterMix;
+    public Dropdown resolutionDropDown;
+    public GameObject MainMenu;
+    public GameObject OptionsMenu;
+    Resolution[] resolutions;
+
+    void Start(){
+        resolutions=Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex=0;
+        for(int i=0; i<resolutions.Length; i++){
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height){
+                currentResolutionIndex=i;
+            }
+        }
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value=currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
     public void startGame(){
         SceneManager.LoadScene("GameScene");
     }
@@ -26,4 +52,17 @@ public class MainMenuHandler : MonoBehaviour
     public void setFullScreen(bool isFullScreen){
         Screen.fullScreen=isFullScreen;
     }
+    public void setResolution(int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void openOptions(){
+        MainMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+    public void closeOptions(){
+        MainMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+        
 }
